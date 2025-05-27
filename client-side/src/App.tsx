142895ccda9d207902893b3f8ff698c33cbf94bf
Aktasidris <akta.idris@gmail.com>
2025-05-27 18:00:45 +0300

    fixed vercel analytics bug

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -2,27 +2,29 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./router/AppRoutes";
 import Header from "./components/layouts/Header"; // Header'ı import ettik
-import { Analytics } from "@vercel/analytics/next"
 import Footer from "./components/layouts/Footer";
 //import GlobalBackdrop from "./components/common/GlobalBackdrop";
+import { Analytics } from '@vercel/analytics/react';
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import ScrollUp from "./components/layouts/Scroolup";
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col min-h-screen ">
-        <Header />
-        <main className="flex-1 flex justify-center w-full  px-4  min-h-[calc(100vh-<header+footer-height>)] relative">
-          <AppRoutes />
-          <ScrollUp />
-        </main>
-        <Footer></Footer>
-      </div>
-      {/* <GlobalBackdrop /> */}
-      <ToastContainer position="top-right" autoClose={3000} />
-       <Analytics />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <div className="flex flex-col min-h-screen ">
+          <Header />
+          <main className="flex-1 flex justify-center w-full  px-4  min-h-[calc(100vh-<header+footer-height>)] relative">
+            <AppRoutes />
+            <ScrollUp />
+          </main>
+          <Footer></Footer>
+        </div>
+        {/* <GlobalBackdrop /> */}
+        <ToastContainer position="top-right" autoClose={3000} />
+      </BrowserRouter>
+       <Analytics mode="production" />
+    </>
   );
 }
 
